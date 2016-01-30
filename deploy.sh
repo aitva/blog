5ac529331caae7a36241c7ac7abe5f82830fd8fa
aitva <arod.louis@gmail.com>
2016-01-30 01:05:26 +0100

    Update deploy.

--- deploy.sh
+++ deploy.sh
@@ -9,7 +9,11 @@ hugo
 git add -A
 
 # Commit changes.
-git commit
+msg="rebuilding site `date`"
+if [ $# -eq 1 ]
+      then msg="$1"
+fi
+git commit -m "$msg"
 
 # Push source and build repos.
 git push origin master
